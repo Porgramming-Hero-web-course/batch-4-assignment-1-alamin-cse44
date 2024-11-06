909d9e4857fce4bbdd0db86a2f3f9db088052d60
alamin-cse44 <alamincse44@gmail.com>
2024-11-07 02:16:11 +0600

    Solve: problem-8

--- problem-8.ts
+++ problem-8.ts
@@ -1,7 +1,18 @@
 {
-    // 
+  //
 
-    
+  const validateKeys = <T, K extends keyof T>(obj: T, keys: K[]): boolean => {
+    for (const key of keys) {
+      if (!(key in obj)) {
+        return false;
+      }
+    }
 
-    //
+    return true;
+  };
+
+  const person = { name: "Alice", age: 25, email: "alice@example.com" };
+  console.log(validateKeys(person, ["name", "age"]));
+
+  //
 }
