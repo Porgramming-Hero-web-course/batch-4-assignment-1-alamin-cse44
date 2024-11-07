bd6e4e1959c196d318cd99b5e7c385c7a0983e33
alamin-cse44 <alamincse44@gmail.com>
2024-11-07 15:57:24 +0600

    Refactor: problem-6

--- problem-6.ts
+++ problem-6.ts
@@ -1,13 +1,19 @@
 {
   //
 
-  interface Profile {
-    name?: string;
-    age?: number;
-    email?: string;
-  }
+  // interface Profile {
+  //   name?: string;
+  //   age?: number;
+  //   email?: string;
+  // }
 
-  interface Partial extends Profile {}
+  // interface Partial extends Profile {}
+
+  interface Profile {
+    name: string;
+    age: number;
+    email: string;
+  }
 
   const myProfile: Profile = {
     name: "Alice",
@@ -15,10 +21,11 @@
     email: "alice@example.com",
   };
 
-  const updateProfile = (profile: Profile, partial: Partial): Profile => {
-    profile.age = partial.age;
+  const updateProfile = (profile: Profile, partial: Partial<Profile>): Profile => {
+    // profile.age = partial.age;
+    // return profile;
 
-    return profile;
+    return {...profile, ...partial}
   };
 
   console.log(updateProfile(myProfile, { age: 26 }));
