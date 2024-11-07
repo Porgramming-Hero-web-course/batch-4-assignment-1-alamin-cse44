8c171ce5cd1c25e62996bdcd081103c0bed5bf50
alamin-cse44 <alamincse44@gmail.com>
2024-11-07 15:36:39 +0600

    Update: problem-5

--- problem-5.ts
+++ problem-5.ts
@@ -11,11 +11,14 @@
         age: 30
     };
 
-    const getProperty = <T, K extends keyof T>(person: T, key: K): T[K] => {
+    // constraints for requering the name
+    const getProperty = <T extends {name: string}, K extends keyof T>(person: T, key: K): T[K] => {
         return person[key];
     }
 
-    console.log(getProperty(person, "name"))
+    console.log(getProperty(person, "name"));
+    console.log(getProperty({name: "x", address: "x-home"}, "address"));
+    console.log(getProperty({name: "y",age: 40, address: "y-home"}, "age"));
 
 
 
